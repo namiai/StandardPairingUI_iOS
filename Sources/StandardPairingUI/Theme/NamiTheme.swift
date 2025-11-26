875c7e963d247adba5d0e086e2ba19449c1fed23
Tran Viet Hoang <hoang@nami.ai>
2025-11-26 16:38:37 +0700

    fix the import for NamiTheme

--- Sources/StandardPairingUI/Theme/NamiTheme.swift
+++ Sources/StandardPairingUI/Theme/NamiTheme.swift
@@ -1,7 +1,7 @@
 // Copyright (c) nami.ai
 
 import SwiftUI
-import NamiPairingFramework
+import NamiSharedUIElements
 
 public struct NamiTheme: ThemeProtocol {
 
@@ -10,25 +10,25 @@ public struct NamiTheme: ThemeProtocol {
     }
 
     // Fonts
-    public var headline1: Font = NamiPairingFramework.NamiTextStyle.headline.font
+    public var headline1: Font = NamiSharedUIElements.NamiTextStyle.headline.font
 
-    public var headline2: Font = NamiPairingFramework.NamiTextStyle.headline2.font
+    public var headline2: Font = NamiSharedUIElements.NamiTextStyle.headline2.font
 
-    public var headline3: Font = NamiPairingFramework.NamiTextStyle.headline3.font
+    public var headline3: Font = NamiSharedUIElements.NamiTextStyle.headline3.font
 
-    public var headline4: Font = NamiPairingFramework.NamiTextStyle.headline4.font
+    public var headline4: Font = NamiSharedUIElements.NamiTextStyle.headline4.font
 
-    public var headline5: Font = NamiPairingFramework.NamiTextStyle.headline5.font
+    public var headline5: Font = NamiSharedUIElements.NamiTextStyle.headline5.font
 
-    public var headline6: Font = NamiPairingFramework.NamiTextStyle.headline6.font
+    public var headline6: Font = NamiSharedUIElements.NamiTextStyle.headline6.font
 
-    public var paragraph1: Font = NamiPairingFramework.NamiTextStyle.paragraph1.font
+    public var paragraph1: Font = NamiSharedUIElements.NamiTextStyle.paragraph1.font
 
-    public var paragraph2: Font = NamiPairingFramework.NamiTextStyle.paragraph2.font
+    public var paragraph2: Font = NamiSharedUIElements.NamiTextStyle.paragraph2.font
 
-    public var small1: Font = NamiPairingFramework.NamiTextStyle.small.font
+    public var small1: Font = NamiSharedUIElements.NamiTextStyle.small.font
 
-    public var small2: Font = NamiPairingFramework.NamiTextStyle.small2.font
+    public var small2: Font = NamiSharedUIElements.NamiTextStyle.small2.font
 
 
     // Colors
@@ -37,11 +37,11 @@ public struct NamiTheme: ThemeProtocol {
 
     public var navigationBarColor: Color? = nil
 
-    public var primaryActionButtonStyle: any ButtonStyle = NamiPairingFramework.NamiActionButtonStyle(rank: .primary)
+    public var primaryActionButtonStyle: any ButtonStyle = NamiSharedUIElements.NamiActionButtonStyle(rank: .primary)
 
-    public var secondaryActionButtonStyle: any ButtonStyle = NamiPairingFramework.NamiActionButtonStyle(rank: .secondary)
+    public var secondaryActionButtonStyle: any ButtonStyle = NamiSharedUIElements.NamiActionButtonStyle(rank: .secondary)
 
-    public var tertiaryActionButtonStyle: any ButtonStyle = NamiPairingFramework.NamiActionButtonStyle(rank: .tertiary)
+    public var tertiaryActionButtonStyle: any ButtonStyle = NamiSharedUIElements.NamiActionButtonStyle(rank: .tertiary)
 
-    public var destructiveActionButtonStyle: any ButtonStyle = NamiPairingFramework.NamiActionButtonStyle(rank: .destructive)
+    public var destructiveActionButtonStyle: any ButtonStyle = NamiSharedUIElements.NamiActionButtonStyle(rank: .destructive)
 } 
