117c33c65f5e49adbe1a2e049094a81e5a8233a2
Ilya <uin153974748@gmail.com>
2026-02-16 17:18:12 +1000

    module prior resolution

--- Sources/StandardPairingUI/Animations.swift
+++ Sources/StandardPairingUI/Animations.swift
@@ -5,28 +5,38 @@ import Lottie
 import SharedAssets
 
 extension Animations {
+    
+    private func animationLocalFirst(_ name: String) -> LottieAnimation? {
+        let prioritisedBundle: Bundle!
+        #if SWIFT_PACKAGE
+        prioritisedBundle = .module
+        #else
+        prioritisedBundle = .main
+        #endif
+        return LottieAnimation.named(name, bundle: prioritisedBundle) ?? animationNamed(name)
+    }
 
     // MARK: - Pulse Dark Blue
 
-    var alarmPodPulseDarkBlue: LottieAnimation! { animationNamed("Pulse Dark Blue_Alarm Pod _ Security Pod") }
-    var sensePodPulseDarkBlue: LottieAnimation! { animationNamed("Pulse Dark Blue_SensePod") }
-    var sensePlugDEPulseDarkBlue: LottieAnimation! { animationNamed("Pulse Dark Blue_SensePlug DE") }
-    var sensePlugFRPulseDarkBlue: LottieAnimation! { animationNamed("Pulse Dark Blue_SensePlug FR") }
-    var sensePlugUKPulseDarkBlue: LottieAnimation! { animationNamed("Pulse Dark Blue_SensePlug UK") }
-    var sensePlugUSPulseDarkBlue: LottieAnimation! { animationNamed("Pulse Dark Blue_SensePlug US") }
-    var sensePlugJPPulseDarkBlue: LottieAnimation! { animationNamed("Pulse Dark Blue_SensePlug JP") }
+    var alarmPodPulseDarkBlue: LottieAnimation! { animationLocalFirst("Pulse Dark Blue_Alarm Pod _ Security Pod") }
+    var sensePodPulseDarkBlue: LottieAnimation! { animationLocalFirst("Pulse Dark Blue_SensePod") }
+    var sensePlugDEPulseDarkBlue: LottieAnimation! { animationLocalFirst("Pulse Dark Blue_SensePlug DE") }
+    var sensePlugFRPulseDarkBlue: LottieAnimation! { animationLocalFirst("Pulse Dark Blue_SensePlug FR") }
+    var sensePlugUKPulseDarkBlue: LottieAnimation! { animationLocalFirst("Pulse Dark Blue_SensePlug UK") }
+    var sensePlugUSPulseDarkBlue: LottieAnimation! { animationLocalFirst("Pulse Dark Blue_SensePlug US") }
+    var sensePlugJPPulseDarkBlue: LottieAnimation! { animationLocalFirst("Pulse Dark Blue_SensePlug JP") }
 
-    var widarPulseDarkBlue: LottieAnimation! { animationNamed("Pulse Dark Blue_WiDAR") }
+    var widarPulseDarkBlue: LottieAnimation! { animationLocalFirst("Pulse Dark Blue_WiDAR") }
 
-    var wifiSensorDEPulseDarkBlue: LottieAnimation! { animationNamed("Pulse Dark Blue_WiFi DE") }
-    var wifiSensorFRPulseDarkBlue: LottieAnimation! { animationNamed("Pulse Dark Blue_WiFi FR") }
-    var wifiSensorJPPulseDarkBlue: LottieAnimation! { animationNamed("Pulse Dark Blue_WiFi JP") }
-    var wifiSensorUKPulseDarkBlue: LottieAnimation! { animationNamed("Pulse Dark Blue_WiFi UK") }
-    var wifiSensorUSPulseDarkBlue: LottieAnimation! { animationNamed("Pulse Dark Blue_WiFi US") }
+    var wifiSensorDEPulseDarkBlue: LottieAnimation! { animationLocalFirst("Pulse Dark Blue_WiFi DE") }
+    var wifiSensorFRPulseDarkBlue: LottieAnimation! { animationLocalFirst("Pulse Dark Blue_WiFi FR") }
+    var wifiSensorJPPulseDarkBlue: LottieAnimation! { animationLocalFirst("Pulse Dark Blue_WiFi JP") }
+    var wifiSensorUKPulseDarkBlue: LottieAnimation! { animationLocalFirst("Pulse Dark Blue_WiFi UK") }
+    var wifiSensorUSPulseDarkBlue: LottieAnimation! { animationLocalFirst("Pulse Dark Blue_WiFi US") }
 
     // MARK: - Pulse White
 
-    var doorSensorPulseWhite: LottieAnimation! { animationNamed("Pulse White_Door Sensor") }
-    var keypadPulseWhite: LottieAnimation! { animationNamed("Pulse White_Keypad") }
-    var motionSensorPulseWhite: LottieAnimation! { animationNamed("Pulse White_Motion Sensor") }
+    var doorSensorPulseWhite: LottieAnimation! { animationLocalFirst("Pulse White_Door Sensor") }
+    var keypadPulseWhite: LottieAnimation! { animationLocalFirst("Pulse White_Keypad") }
+    var motionSensorPulseWhite: LottieAnimation! { animationLocalFirst("Pulse White_Motion Sensor") }
 }
